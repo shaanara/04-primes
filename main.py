82228cb7f574cfc1f8cc4a0742e9eb918cbb32ce
Shanice Cyprienne <shanice.cyprienne@edu.esiee.fr>
2025-09-29 13:49:42 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
+"""definition d'une fonction qui teste si un nombre est premier"""
 from math import sqrt
-
-#### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
-
+    """fonction qui teste si un nombre est premier"""
+    if p<2:
+        return False
+    for i in range(2,int(sqrt(p)+1)):
+        if p%i==0 and p!=i:
+            return False
+        return True
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """fonction qui renvoie les nombres premiers inférieurs à 100"""
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
-
 if __name__ == "__main__":
     main()
